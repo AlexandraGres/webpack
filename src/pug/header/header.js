cf26d397237204f3a1ab1c08386d535d2ab0884b
Oleksandra Hresniuk <meonis89@gmail.com>
2021-08-12 13:45:04 +0300

    Added content and styles to home page

--- src/pug/header/header.js
+++ src/pug/header/header.js
@@ -2,5 +2,5 @@ const page_url = window.location.pathname
 
 if (page_url === '/') {
   const welcome_block = document.getElementById('welcome_block')
-  welcome_block.innerHTML = '<h2 class="k-font welcome-block__title-m">Creative Template</h2><h1 class="welcome-block__title-l">Welcome<br>to mogo</h1>'
+  welcome_block.innerHTML = '<h2 class="k-font welcome-block__title-m">Creative Template</h2><h1 class="welcome-block__title-l m-font">Welcome<br>to mogo</h1>'
 }
