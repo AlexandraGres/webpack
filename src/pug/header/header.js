5239c9323b8457a9f158be2122d09fb4554b9327
Oleksandra Hresniuk <meonis89@gmail.com>
2021-08-12 20:47:09 +0300

    Added responsive styles

--- src/pug/header/header.js
+++ src/pug/header/header.js
@@ -1,8 +1,13 @@
 const page_url = window.location.pathname
-
-console.log(page_url);
+const homePageLink = document.getElementById('home_page')
+const teamPageLink = document.getElementById('team_page')
 
 if (page_url === '/' | page_url === '/index.html') {
-  const welcome_block = document.getElementById('welcome_block')
-  welcome_block.innerHTML = '<h2 class="k-font welcome-block__title-m">Creative Template</h2><h1 class="welcome-block__title-l m-font">Welcome<br>to mogo</h1>'
-}
+  const welcomeBlock = document.getElementById('welcome_block')  
+  welcomeBlock.innerHTML = '<h2 class="k-font welcome-block__title-m">Creative Template</h2><h1 class="welcome-block__title-l m-font">Welcome<br>to mogo</h1>'
+  homePageLink.className += ' active'
+} 
+
+if (page_url === '/team.html') {
+  teamPageLink.className += ' active'
+} 
